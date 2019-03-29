48788e002241a0738c61a0ad70f2522c36424dd5
ericmartineau <smartytime@gmail.com>
2019-03-29 09:52:04 -0700

    Fixing issue with Package.swift file

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "Reswift-Thunk",
+    name: "ReSwift-Thunk",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Reswift-Thunk",
-            targets: ["Reswift-Thunk"]),
+            name: "ReSwift-Thunk",
+            targets: ["ReSwift-Thunk"]),
     ],
     dependencies: [
-        
+        .package(url: "git@github.com:SunnyApp/ReSwift", from: "5.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Reswift-Thunk",
-            dependencies: [],
-            path: "Reswift-Thunk"
+            name: "ReSwift-Thunk",
+            dependencies: ["ReSwift"],
+            path: "ReSwift-Thunk"
             )
         
     ]
