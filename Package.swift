186869a24554a1bc1aab7a0d817911d45eae305e
ericmartineau <smartytime@gmail.com>
2019-03-29 09:31:02 -0700

    Fixing versions

--- Package.swift
+++ Package.swift
@@ -1,26 +1,26 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Reswift-Thunk",
+    name: "ReSwift-Thunk",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Reswift-Thunk",
-            targets: ["Reswift-Thunk"]),
+            name: "ReSwift-Thunk",
+            targets: ["ReSwift-Thunk"]),
     ],
     dependencies: [
-        
+        .package(url: "git@github.com:SunnyApp/ReSwift.git", from: "5.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Reswift-Thunk",
+            name: "ReSwift-Thunk",
             dependencies: [],
-            path: "Reswift-Thunk"
+            path: "ReSwift-Thunk"
             )
         
     ]
